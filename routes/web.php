8bb4ef739392b3c72743cbe9de004720d60e12c0
Yuji <ss.yuji1012@gmail.com>
2025-11-25 04:37:30 +0900

    ルーティングの追加

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\InquiryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/inquiries', [InquiryController::class, 'index']);
+Route::post('/inquiries',[InquiryController::class, 'post']);
+Route::get('/inquiries', [InquiryController::class, 'complete']);
+
+
+
