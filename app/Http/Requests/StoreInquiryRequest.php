40532725ee5d901315a4dd9e73dda776de9b462b
Yuji <ss.yuji1012@gmail.com>
2025-11-27 18:01:14 +0900

    requestファイルの追加

---
+++ app/Http/Requests/StoreInquiryRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreInquiryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|max:255',
+            'email' => 'required|max:255',
+            'type' => 'required',
+            'content' => 'required|max:1000',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'name' => '名前',
+            'email' => 'メールアドレス',
+            'type' => '種別',
+            'content' => 'お問い合わせ内容',
+        ];
+    }
+}
