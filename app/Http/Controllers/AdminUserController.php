b316e2d2efe6e8d3bc7c6f6477a0164ac0248235
Yuji <ss.yuji1012@gmail.com>
2025-12-05 18:49:34 +0900

    管理ユーザーコントローラの追加

---
+++ app/Http/Controllers/AdminUserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAdminUserRequest;
+use App\Http\Requests\UpdateAdminUserRequest;
+use App\Models\User;
+
+class AdminUserController extends Controller
+{
+    public function create()
+    {
+        return view('admin.users.create');
+    }
+
+    public function index()
+    {
+        $users = User::all();
+
+        return view('admin.users.index', ['users' => $users]);
+    }
+
+    public function store(StoreAdminUserRequest $request)
+    {
+        $user = User::create($request->validated());
+
+        return redirect()->route('admin.users.index')
+                         ->with('success', 'ユーザー登録が完了しました。');
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+
+        return view('admin.users.show', ['user' => $user]);
+    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+
+        return view('admin.users.edit', ['user' => $user]);
+    }
+
+    public function update(UpdateAdminUserRequest $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $user->update($request->all());
+
+        return redirect()->route('admin.users.index')
+                         ->with('success', 'ユーザーを更新しました。');
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+
+        $user->delete();
+
+        return redirect()->route('admin.users.index')
+                         ->with('success', 'ユーザーを削除しました。');
+    }
+}
