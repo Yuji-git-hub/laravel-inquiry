2901a8f03b62cdd7ca1a1f9e2ccf753f76f99ab5
Yuji <ss.yuji1012@gmail.com>
2026-01-28 07:28:06 +0900

    AdminUserStoreControllerファイルの作成

---
+++ app/Http/Controllers/AdminUser/AdminUserStoreController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\AdminUser;
+
+use App\Application\AdminUser\Create\CreateAdminUserUseCase;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreAdminUserRequest;
+
+final class AdminUserStoreController extends Controller
+{
+    public function __construct(
+        private CreateAdminUserUseCase $useCase
+    )
+    {}
+
+    public function __invoke(StoreAdminUserRequest $request)
+    {
+        $this->useCase->handle(
+            name: $request->name(),
+            email: $request->email(),
+            password: $request->password(),
+        );
+
+        return redirect()
+            ->route('admin.users.index')
+            ->with('success', '管理ユーザーを登録しました。');
+    }
+}
